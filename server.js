9baf23875a30a0e8072e4443f4d1250bd3189951
Just-A-Pixel <Raj.Anand0511@gmail.com>
2021-11-14 11:25:08 +0530

    api

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const express = require("express");
 const clothes = require("./routes/clothes");
 const auth = require("./routes/auth");
 const mongoose = require("mongoose");
-
+const cors = require("cors")
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use("/api/clothes", clothes);
 app.use("/api/auth", auth);
